e6aa5aa8d6acf1cab2d5a7e0424f8a1dade6fea2
Jamie Maynard <james.maynard@digital.cabinet-office.gov.uk>
2020-04-16 12:16:09 +0100

    Aded icons and more feedback
    Also added a test function to the start script so you can see
    what the variables used to start docer will be.  Should resolve
    issues with having both environment variables set and commandline
    variables seet.

--- start.sh
+++ start.sh
@@ -1,48 +1,107 @@
 #!/bin/bash
 
-if [ -z $PSQL_PASSWORD ]; then
-    PSQL_PASSWORD=docker
-fi
-if [ -z $PSQL_PORT ]; then
-    PSQL_PORT=5432
-fi
-if [ -z $PGADMIN_PORT ]; then
-    PGADMIN_PORT=5050
-fi
-if [ -z $PGADMIN_USER ]; then
-    PGADMIN_USER=test@test.com
-fi
-if [ -z $PGADMIN_PASSWORD ]; then
-    PGADMIN_PASSWORD=8lScP10eY1L3oeXbBM1E
+# Define colors and styles
+normal="\033[0m"
+bold="\033[1m"
+green="\e[32m"
+red="\e[31m"
+yellow="\e[93m"
+
+# Define Icons (if in doubt try nerd font... then emoji.. finally nothing)
+if [[ "$NERD_FONT" == 'true' ]]; then
+  warn_icon="\uf071 "
+  docker_icon="\uf308 "
+  whale_icon="🐋"
+elif [[ "$EMOJI" == 'false' ]]; then
+  warn_icon="!"
+  whale_icon=""
+  docker_icon=""
+else
+  warn_icon="⚠️"
+  whale_icon="🐋"
+  docker_icon="🐳"
 fi
 
-if groups $(whoami) |grep docker > /dev/null 2>&1; then
-    DOCKER_CMD="docker"
+sudowarn() {
+    echo -e "$red╒═════════════════════════════════════════════════════════╕$normal"
+    echo -e "$red│$normal $yellow$warn_icon $normal  WARNING                                             $red│$normal"
+    echo -e "$red├─────────────────────────────────────────────────────────┤$normal"
+    echo -e "$red│$normal You're not part of the docker group so you'll need      $red│$normal"
+    echo -e "$red│$normal sudo access in order to run this script.  If you don't  $red│$normal"
+    echo -e "$red│$normal have sudo access please ask an admin to add you to the  $red│$normal"
+    echo -e "$red│$normal the docker group.                                       $red│$normal"
+    echo -e "$red╘═════════════════════════════════════════════════════════╛$normal\n"
+}
+
+if [ -z $PSQL_PASSWORD ]; then
+    PSQL_PASSWORD=docker
+    PSQLPASS='default'
 else
-    DOCKER_CMD="sudo docker"
+    PSQLPASS='current'
 fi
+
+if [ -z $PSQL_PORT ]; then
+    PSQL_PORT=5432
+    PSQLPO='default'
+else
+    PSQLPO='current'
+fi
+
+if [ -z $PGADMIN_PORT ]; then
+    PGADMIN_PORT=5050
+    PGAPORT='default'
+else
+    PGAPORT='current'
+fi
+
+if [ -z $PGADMIN_USER ]; then
+    PGADMIN_USER=test@test.com
+    PGAUSER='default'
+else
+    PGAUSER='current'
+fi
+
+if [ -z $PGADMIN_PASSWORD ]; then
+    PGADMIN_PASSWORD=8lScP10eY1L3oeXbBM1E
+    PGAPASS='default'
+else
+    PGAPASS='current'
+fi
+
+TEST=false
 
 usage() {
     echo -e "Usage:"
     echo -e "  --psql-password     Sets the PostgreSQL password"
-    echo -e "                      (The default is 'docker')\n"
+    echo -e "                      (The $PSQLPASS is '$PSQL_PASSWORD')\n"
     echo -e "  --psql-port         Sets the PostgreSQL port"
-    echo -e "                      (The default is '5432')\n"
+    echo -e "                      (The $PSQLPO is '$PSQL_PORT')\n"
     echo -e "  --pgadmin-port      Sets the port for PGAdmin 4"
-    echo -e "                      (The default is '5050')\n"
+    echo -e "                      (The $PGAPORT is '$PGADMIN_PORT')\n"
     echo -e "  --pgadmin-user      Sets the default login user for PGAdmin 4"
-    echo -e "                      (The default is 'test@test.test')\n"
+    echo -e "                      (The $PGAUSER is '$PGADMIN_USER')\n"
     echo -e "  --pgadmin-password  Sets the password for the default login for PGAdmin 4"
-    echo -e "                      (The default is '8lScP10eY1L3oeXbBM1E')\n"
+    echo -e "                      (The $PGAPASS is '$PGADMIN_PASSWORD')\n"
+    echo -e "  -h  --help          Shows this usage message"
+    echo -e "  -t   --test         Shows hows docker will be configrued but won't run docker"
 }
 
-while [ "$1" != "" ]; do                                                   
-    case $1 in                                                             
+showtest() {
+    echo -e "Docker will be configured as follows:"
+    echo -e "   PSQL_PASSWORD    = $PSQL_PASSWORD"
+    echo -e "   PSQL_PORT        = $PSQL_PORT"
+    echo -e "   PGADMIN_PORT     = $PGADMIN_PORT"
+    echo -e "   PGADMIN_USER     = $PGADMIN_USER"
+    echo -e "   PGADMIN_PASSWORD = $PGADMIN_PASSWORD"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
         --psql-password )       shift
                                 PSQL_PASSWORD=$1
-                                ;;                                         
+                                ;;
         --psql-port )           shift
-                                PSQL_PORT=$1                            
+                                PSQL_PORT=$1
                                 ;;
         --pgadmin-port )        shift
                                 PGADMIN_PORT=$1
@@ -53,29 +112,46 @@ while [ "$1" != "" ]; do
         --pgadmin-password )    shift
                                 PGADMIN_PASSWORD=$1
                                 ;;
-        -h | --help )           usage                                   
-                                exit                                    
-                                ;;                                      
-        * )                     echo -e "Unknown option...\n"
-                                usage                                   
-                                exit 1                                  
-    esac                                                                
-    shift                                                               
-done       
+        -h | --help )           usage
+                                exit
+                                ;;
+        -t | --test )           TEST=true
+                                ;;
+        --show-warn )           sudowarn;
+                                exit
+                                ;;
+        * )                     echo -e "Unknown option $1...\n"
+                                usage
+                                exit 1
+    esac
+    shift
+done     
+
+if groups $(whoami) |grep docker > /dev/null 2>&1; then
+    DOCKER_CMD="docker"
+else
+    DOCKER_CMD="sudo docker"
+    showwarn
+fi
+
+if [ $TEST == "true" ]; then
+    showtest
+    exit
+fi
 
 docker volume ls | grep pgdata > /dev/null 2>&1
 if [[ $? != 0 ]]; then
-    docker volume create pgdata
+    docker volume create pgdata > /dev/null 2>&1
 fi
 
 docker volume ls | grep pga4data > /dev/null 2>&1
 if [[ $? != 0 ]]; then
-    docker volume create pga4data
+    docker volume create pga4data > /dev/null 2>&1
 fi
 
 docker network list |grep pgnetwork > /dev/null 2>&1
 if [[ $? != 0 ]]; then
-    docker network create --driver bridge pgnetwork
+    docker network create --driver bridge pgnetwork > /dev/null 2>&1
 fi
 
 cat << EOF > /tmp/.pgadmin-env.list
@@ -84,6 +160,7 @@ PGADMIN_DEFAULT_PASSWORD=$PGADMIN_PASSWORD
 PGADMIN_LISTEN_PORT=$PGADMIN_PORT
 EOF
 
+echo -e " $whale_icon Starting PostgreSQL..."
 $DOCKER_CMD run --rm --name postgres \
     --network pgnetwork \
     --hostname postgres \
@@ -92,6 +169,7 @@ $DOCKER_CMD run --rm --name postgres \
     --volume pgdata:/var/lib/postgresql/data \
     postgres 1> /dev/null
 
+echo -e " $whale_icon Starting PGAdmin 4..."
 $DOCKER_CMD run --rm --name pgadmin4 \
     -d -p $PGADMIN_PORT:$PGADMIN_PORT \
     -v pga4data:/var/lib/pgadmin \
@@ -100,3 +178,6 @@ $DOCKER_CMD run --rm --name pgadmin4 \
     --network pgnetwork \
     dpage/pgadmin4 1> /dev/null
 
+rm /tmp/.pgadmin-env.list
+
+echo -e " $docker_icon PostgreSQL and PGAdmin 4 are now running in Docker!"
--- stop.sh
+++ stop.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+# Define colors and styles
+normal="\033[0m"
+bold="\033[1m"
+green="\e[32m"
+red="\e[31m"
+yellow="\e[93m"
+
+# Define Icons (if in doubt try nerd font... then emoji.. finally nothing)
+if [[ "$NERD_FONT" == 'true' ]]; then
+  stop_icon="\uf46e"
+  trash_icon="\uf1f8"
+  knet_icon="\uf700"
+  dnet_icon="\uf701"
+  done_icon="\uf634"
+  hd_icon="\uf7c9"
+elif [[ "$EMOJI" == 'false' ]]; then
+  trash_icon=''
+  stop_icon=''
+  net_icon=''
+  done_icon=''
+  hd_icon=''
+else
+  hd_icon="💾"
+  trash_icon="🗑️"
+  stop_icon="🛑"
+  knet_icon="🔌"
+  dnet_icon="🔌"
+  done_icon="✔️"
+fi
+
 if [ -z $REMOVE_VOLS ]; then
     REMOVE_VOLS=false
 fi
@@ -33,13 +63,21 @@ while [ "$1" != "" ]; do
     shift
 done
 
+echo -e " $stop_icon  Stopping PostreSQL and PGAdmin..."
 docker stop postgres pgadmin4 1> /dev/null
 
-if [ REMOVE_VOLS == 'true' ]; then
+if [ $REMOVE_VOLS == 'true' ]; then
+    echo -e " $trash_icon $red Removing PostgreSQL and PGAdmin data $normal"
     docker volume rm pga4data > /dev/null 2>&1
     docker volume rm pgdata > /dev/null 2>&1
+else
+    echo -e " $hd_icon  Persisting Data by keeping docker volumes."
 fi
 
-if [ REMOVE_NET == 'true' ]; then
+if [ $REMOVE_NET == 'true' ]; then
+    echo -e " $dnet_icon $red Removing PGNetwork $normal"
     docker network rm pgnetwork > /dev/null 2>&1
+else
+    echo -e " $knet_icon  Keeping PGNetwork active."
 fi
+echo -e " $done_icon  All done."
